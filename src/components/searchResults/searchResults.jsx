3df7922a434816e943d7a0ef0485fb527dadf06c
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-03-09 23:19:27 -0800

    added get and set local storage funcs

--- src/components/searchResults/searchResults.jsx
+++ src/components/searchResults/searchResults.jsx
@@ -1,6 +1,29 @@
+import { useEffect, useState } from "react";
 import RecipeCard from "../recipeCard/recipeCard";
 import styles from "./searchResults.module.css";
 const SearchResults = ({ recipes }) => {
+  const [favoriteMeals, setFavoriteMeals] = useState([]);
+  useEffect(() => {
+    const itemsFromLocal = localStorage.getItem("favorites");
+    setFavoriteMeals(JSON.parse(itemsFromLocal));
+  }, []);
+  const handleClick = (clickedMealName) => {
+    const itemsFromLocal = JSON.parse(localStorage.getItem("favorites"));
+    if (itemsFromLocal) {
+      if (!itemsFromLocal.includes(clickedMealName)) {
+        localStorage.setItem(
+          "favorites",
+          JSON.stringify([...itemsFromLocal, clickedMealName])
+        );
+        setFavoriteMeals([...itemsFromLocal, clickedMealName]);
+      } else {
+        alert("It is already in favorites :)");
+      }
+    } else {
+      localStorage.setItem("favorites", JSON.stringify([clickedMealName]));
+      setFavoriteMeals([clickedMealName]);
+    }
+  };
   return (
     <div className={styles.searchResults}>
       {recipes &&
@@ -10,6 +33,8 @@ const SearchResults = ({ recipes }) => {
             data={recipe.recipe}
             key={i}
             btnText="see the recipe"
+            handleClick={handleClick}
+            favoritesFromLocalStorage={favoriteMeals}
           />
         ))}
     </div>
